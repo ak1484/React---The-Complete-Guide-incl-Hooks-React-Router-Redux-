4444d1ef62ac512eb1e22d1071cdb8c6bf24f404
ak1484 <ankitsaharan1484@gmail.com>
2021-05-18 12:41:38 +0530

    How Component Functions Are Executed

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -2,14 +2,16 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
-  const clickHandler=()=>{
-     console.log("clicked !")
-  }
+  let title = props.title;
+  const clickHandler = () => {
+    title = "updated";
+    console.log(title);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
